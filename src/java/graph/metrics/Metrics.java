69f5372fd173f60df1dad889313c1ea3d2aefaec
Nurbolat69 <nurbolatsulejmenov2@gmail.com>
2025-11-02 20:19:20 +0500

    Delete src/java/graph directory

--- src/java/graph/metrics/Metrics.java
+++
@@ -1,9 +0,0 @@
-package graph.metrics;
-
-public interface Metrics {
-    void start();
-    void stop();
-    void inc(String counter);
-    long getCounter(String counter);
-    long elapsedNanos();
-}
